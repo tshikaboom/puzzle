63333c47855267568df03a572e20b970fdd887cc
BIELLE BENJAMIN <2900825@ari-41-303-12.infop6.jussieu.fr>
2013-02-12 19:59:04 +0100

    Creation chemin

---
+++ chemin.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "structs.h"
+
+Position chemin (int i, ,int choix)
+{
+  Position pos_suivant;
+  
+  /*En Spirale*/
+  int tabI_spirale[10] = {1,0,0,1,2,2,2,1,0};
+  int tabJ_spirale[10] = {1,1,2,2,2,1,0,0,0};
+  
+  /*En Z*/
+  int tabI_Z[10] = {0,1,2,2,1,0,0,1,2};
+  int tabJ_Z[10] = {0,0,0,1,1,1,2,2,2};
+  
+  
+  switch(choix)
+    {
+    case 1 :
+      {
+	pos_suivant.x = tabI_spirale[i+1];
+	pos_suivant.y = tabJ_spirale[i+1];
+	
+	return pos_suivant;
+      }
+      break;
+      
+    case 2 :
+      {
+	pos_suivant.x = tabI_Z[i+1];
+	pos_suivant.y = tabJ_Z[i+1];
+	
+	return pos_suivant;
+      }
+      break;
+
+    default :
+      {
+	printf("Choix du chemin non valide\n");
+	pos_suivant.x = -1;
+	pos_suivant.y = -1;
+	break;
+      }
+    }
+  return pos_suivant;
+}
---
+++ chemin.h
@@ -0,0 +1,7 @@
+#ifnedef CHEMIN_H
+#define CHEMIN_H
+
+/*Defini le chemin de parcours de la grille*/
+Position chemin (int i, int choix);
+
+#endif
