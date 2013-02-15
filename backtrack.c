c6e5990d62f0a31bbd33547974522b73ef2688f6
Oskar Viljasaar <oskar.viljasaar@eesti.ee>
2013-02-15 17:57:13 +0100

    Modification de backtrack pour utiliser la nouvelle structure plateau

--- backtrack.c
+++ backtrack.c
@@ -14,6 +14,7 @@
 
 /*Algorithme de backtracking*/
 int backtrack (Carte *plateau[][TAILLE],Carte tabCarte[], int largeur, int hauteur, int nombre_de_carte,int indice_chemin,int choix)
+int backtrack (Plateau *plateau ,Carte tabCarte[], int nombre_de_carte,int indice_chemin,int choix)
 {
   int nombre_de_solution;
   int carte; /*numero de la carte*/
@@ -31,24 +32,24 @@ int backtrack (Carte *plateau[][TAILLE],Carte tabCarte[], int largeur, int haute
       /* position_case = caseDepart(largeur,hauteur);               Placement optimal -> On le fera dans le main pour permettre a backtrack d'etre plus libre !!*/
       position_case = chemin(-1,choix); /*retourne a la position initiale*/
       
-      if ( (plateau[position_case.x][position_case.y] == ((largeur*hauteur)-1)) && (tabCarte[(largeur*hauteur)-1].rotated == 4) )
+      if ( (plateau->tab[position_case.x][position_case.y] == ((plateau->largeur*plateau->hauteur)-1)) && (tabCarte[(plateau->largeur*plateau->hauteur)-1].rotated == 4) )
 	return TRUE;
     }
 
-  for (carte = 0; carte < (largeur*hauteur); carte++)
+  for (carte = 0; carte < (plateau->largeur*plateau->hauteur); carte++)
     {
-      if ( (tabCarte[carte].sur_plateau != 1) && (cartePossible(tabCarte[carte],plateau[][TAILLE],position_case.x,position_case.y,largeur,hauteur)) )
+      if ( (tabCarte[carte].sur_plateau != 1) && (cartePossible(tabCarte[carte],plateau, position_case.x,position_case.y)) )
 	{
 	  tabCarte[carte].sur_plateau = 1;/*On dit qu'elle est sur le plateau*/
 	  tabCarte[carte].x = position_case.x; /*Position de la carte a la position courante*/
 	  tabCarte[carte].y = position_case.y; /* idem */
 
-	  if ( backtrack(plateau[][TAILLE],tabCarte[],largeur,hauteur,nombre_de_carte-1,indice_chemin+1,choix) )
+	  if ( backtrack(plateau,tabCarte[],nombre_de_carte-1,indice_chemin+1,choix) )
 	    {
 	      return TRUE;
 	    }
 
-	  plateau[position_case.x][position_case.y] = -1; /*reinitialisation des valeurs fausses*/
+	  plateau->tab[position_case.x][position_case.y] = NULL; /*reinitialisation des valeurs fausses*/
 	  tabCarte[carte].sur_plateau = 0; /*on l'enleve du plateau*/
 	  tabCarte[carte].rotated = 4;/*reinitialisation de la rotation de la carte a zero*/
 	  nombre_de_carte++;	    
