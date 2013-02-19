9f3db21081cdb2b27e8b97c009e3607ab10bac56
BIELLE BENJAMIN <2900825@ari-41-303-08.infop6.jussieu.fr>
2013-02-19 18:44:55 +0100

    Test emacs -nw

--- backtrack.h
+++ backtrack.h
@@ -2,7 +2,7 @@
 #define BACKTRACK_H
 #include "structs.h"
 
-
+/****************************/
 /*Algorithme de backtracking*/
 int backtrack (Plateau *plateau,Carte tabCarte[], int nombre_de_carte,int indice_chemin,int choix);
 
