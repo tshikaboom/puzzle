3ba81712958b2b6dc8a8b5bcda1cc18c94f55b82
Oskar Viljasaar <oskar.viljasaar@eesti.ee>
2013-04-09 20:25:30 +0200

    parser.h: fix d'un typo Carte** -> Carte*

--- include/parser.h
+++ include/parser.h
@@ -20,9 +20,12 @@ void error(char* errorMessage);
 int readCard(char* stringCard, Carte* current);
 
 /*Carte** an array containing the cards that were parsed from the file*/
-Carte** parseFile(char* filename,int* hauteur,int* largeur);
+Carte* parseFile(char* filename,int* hauteur,int* largeur);
 
 /*Carte** array of cards containing the cards to be exported*/
 void export(char* filename,int hauteur,int largeur,Carte** stack);
+
+/* fonction magique qui rend un plateau initialise a partir d'un fichier */
+int initPuzzle(char *filename, Plateau *plateau, Carte *stack);
 
 #endif
