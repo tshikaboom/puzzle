e67a2d48be407aeb449c5644a67a7046c902a487
Oskar Viljasaar <oskar.viljasaar@eesti.ee>
2013-04-14 19:03:54 +0200

    chemin.c: Quelques commentages, style de code

--- src/chemin.c
+++ src/chemin.c
@@ -10,43 +10,29 @@ Position chemin (int i,int choix)
 {
   Position pos_suivant;
   
-  /*En Spirale*/
+  /* Parcours en spirale */
   int tabI_spirale[9] = {1,0,0,1,2,2,2,1,0};
   int tabJ_spirale[9] = {1,1,2,2,2,1,0,0,0};
   
-  /*En Z*/
+  /* Parcours lineaire en Z */
   int tabI_Z[9] = {0,1,2,2,1,0,0,1,2};
   int tabJ_Z[9] = {0,0,0,1,1,1,2,2,2};
   
   
-  switch(choix)
-    {
-    case 1 :
-      {
-	pos_suivant.x = tabI_spirale[i];
-	pos_suivant.y = tabJ_spirale[i];
-	
-	/*return pos_suivant; */
-	break;
-      }
-      
-    case 2 :
-      {
-	pos_suivant.x = tabI_Z[i];
-	pos_suivant.y = tabJ_Z[i];
-	
-	/*return pos_suivant; */
-	break;
-      }
-
-    default :
-      {
-	printf("Choix du chemin non valide\n");
-	pos_suivant.x = -1;
-	pos_suivant.y = -1;
-	/*return pos_suivant; */
-	break;
-      }
-    }
+  switch(choix) {
+  case 1:
+    pos_suivant.x = tabI_spirale[i];
+    pos_suivant.y = tabJ_spirale[i];
+    break;
+  case 2:
+    pos_suivant.x = tabI_Z[i];
+    pos_suivant.y = tabJ_Z[i];
+    break;
+  default:
+    printf("Choix du chemin non valide\n");
+    pos_suivant.x = -1;
+    pos_suivant.y = -1;
+    break;
+  }
   return pos_suivant;
 }
