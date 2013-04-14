bd3766d2f1c476ab182d33337f8d06010da39aba
Oskar Viljasaar <oskar.viljasaar@eesti.ee>
2013-04-14 18:38:01 +0200

    contact.h: Clarification des commentaires

--- include/contact.h
+++ include/contact.h
@@ -3,24 +3,25 @@
 
 #include "structs.h"
 
-/* ces fonctions sont centrees sur la carte au point de vue des coordonnees
+/* Ces fonctions sont centrees sur la carte du point de vue des coordonnees
    voir src/contact.c pour plus de precisions
 */
 
-
-/*teste si la case est en contact en haut*/
+/* teste si la carte est positionnable avec la carte deja posee en haut de (i, j)
+   sur le plateau */
 int contact_h(Plateau *plateau, int i, int j, Carte Carte);
 
-/*teste le contact à droite*/
+/* idem pour la droite */
 int contact_d(Plateau *plateau, int i, int j, Carte Carte);
 
-/*teste le contact en bas */
+/* idem pour le bas */
 int contact_b(Plateau *plateau, int i, int j, Carte Carte);
 
-/*teste le contact à gauche*/
+/* idem pour la gauche*/
 int contact_g(Plateau *plateau, int i, int j, Carte Carte);
 
-/*test si la carte est positionnable a la postion donnee*/
+/* Teste si la carte est positionnable a la postion donnee.
+   Utilise les 4 fonctions precedemment definies. */
 int cartePossible(Carte *carte, Plateau *plateau,int i, int j);
 
 #endif
