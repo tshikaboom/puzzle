e12d3e6874fd03c1c0c8af5ae27919d52007e2df
Oskar Viljasaar <oskar.viljasaar@eesti.ee>
2013-04-14 18:43:23 +0200

    backtrack.h: Mini-modification pour mise en forme

--- include/backtrack.h
+++ include/backtrack.h
@@ -2,8 +2,7 @@
 #define BACKTRACK_H
 #include "structs.h"
 
-/****************************/
-/*Algorithme de backtracking*/
+/* Algorithme de backtracking */
 int backtrack (Plateau *plateau,Carte tabCarte[], int nombre_de_carte,int indice_chemin,int choix);
 
 #endif
