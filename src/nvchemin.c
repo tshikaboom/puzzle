66fe08b4b9b6f3b8a7b045ff2a336ae0dcce1000
VIGANO SYLVAIN <3203772@ari-41-303-05.infop6.jussieu.fr>
2013-04-16 20:17:20 +0200

    Bonjour, un nouveau chemin en spirale très rigolo dans une liste chainée simple ! Wazaaaaaa!!!!!!!!!!!!!!!!!!!!

---
+++ src/nvchemin.c
@@ -0,0 +1,100 @@
+/*
+ * File:   main.c
+ * Author: 3203772
+ *
+ * Created on 5 mars 2013, 18:32
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ *
+ */
+
+
+struct chemin{
+//    struct coordonnee point;
+    int x;
+    int y;
+    struct chemin *Suivant;
+};
+
+typedef struct coordonnee Coordonnee;
+typedef struct chemin Chemin;
+
+
+Chemin *nouveau_chemin(int x, int y)
+{
+    Chemin *cellule_initiale;
+    cellule_initiale = (Chemin *) malloc(sizeof(Chemin));
+    // a faire: check du malloc
+    cellule_initiale->Suivant = NULL;
+    
+    cellule_initiale->x = x;
+    cellule_initiale->y = y;
+
+    return cellule_initiale;    
+}
+
+Chemin *rajoute_chemin(Chemin *liste, int x, int y)
+{
+    Chemin *nvcell;
+    nvcell = (Chemin *) malloc(sizeof(Chemin));
+    // a faire: check du malloc
+    nvcell->Suivant = liste;
+    
+    nvcell->x = x;
+    nvcell->y = y;
+
+    // donc la si on veut elargir la liste on prend [[ liste = rajoute_chemin(liste, x, y); ]]    
+    return nvcell;
+    
+}
+
+int min(int a,int b){
+    if (a<b) return a;
+    else return b;
+}
+
+Chemin* initPosition(int n, int p)
+{
+  /*
+    La fonction calcule une somme par coins c'est a dire la case a
+ partir de laquelle on doit changer de direction.
+  */
+    int pas,k,x=n,y=1;
+    int sens=1,j;
+    
+    Chemin* chemin = nouveau_chemin(0,0);
+    
+    for (j=2;j!=n;j+=sens) chemin=rajoute_chemin(chemin,j-1,y-1);
+    
+    for(k=1;k<=min(n,p)-1;k++){
+        
+        pas=sens*(p-k);
+
+        for (j=y;j!=y+pas;j+=sens) chemin=rajoute_chemin(chemin,x-1,j-1);
+        y+=pas;
+        sens=-sens;
+
+        pas=sens*(n-k);
+        for (j=x;j!=x+pas;j+=sens) chemin=rajoute_chemin(chemin,j-1,y-1);
+
+        x+=pas;
+    }
+    chemin=rajoute_chemin(chemin,x-1,y-1);
+    return chemin;
+}
+
+int main(int argc, char** argv) {
+    Chemin *spirale = initPosition(5,4);
+    Chemin *tmp = spirale;
+    while(tmp) {
+        printf("(%d,%d)\n",tmp->x,tmp->y);
+        tmp = tmp->Suivant;
+    }
+    //initPosition(5,4);
+//    initPosition(7,3);
+//    initPosition(3,3);
+}
+
