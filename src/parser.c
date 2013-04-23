a82857ada60958f3bec0539912c9bda9c82c2d02
Oskar Viljasaar <oskar.viljasaar@eesti.ee>
2013-04-23 21:26:08 +0200

    parser: modification de l'importeur pour prendre en charge le nouveau format
    Donc maintenant on a id_HBGD comme format d'IO. voila!

--- src/parser.c
+++ src/parser.c
@@ -5,27 +5,28 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <assert.h>
 
 #include "structs.h"
 #include "init.h"
 
 #define MAX_BUFF 256
 #define INT_OFFSET 48
-#define IMPORT_MASK " %1d%1d%1d%1d"
-#define EXPORT_MASK "%d%d%d%d\n"
+#define IMPORT_MASK " %1d_%1d%1d%1d%1d"
+#define EXPORT_MASK "%d_%d%d%d%d\n"
 
 /*
  * Nettoyer les erreurs avec du perror()
  */
 
-int isEndOfLine(char cara){
+int isEndOfLine(char cara)
+{
   /**
    * checks if input char is EndOfLine.
    * @param char input
    * @return boolean value
    */
-  switch (cara){
+  switch (cara) {
   case '\0':
     return true;
     break;
@@ -42,7 +43,8 @@ int isEndOfLine(char cara){
   return false;
 }
 
-int charToInteger(char cara){
+int charToInteger (char cara)
+{
   /**
    * @param cara Char to be cast to integer
    * @return int result of cast
@@ -50,63 +52,59 @@ int charToInteger(char cara){
   return cara-INT_OFFSET;
 }
 
-int strLength(char* str){
+int strLength(char* str)
+{
   /**
    * @param char* str String to be analyzed
    * @return Input string lenght
    */
   int i=0;
-  while(!isEndOfLine(str[i])) i++;
+  while (!isEndOfLine(str[i])) i++;
   return i;
 }
 
-int stringToInteger(char* str){
+int stringToInteger(char* str)
+{
   /**
    * @param str Input string to be parsed to integer
    * @return int Resulting integer
    */
-  int res=0,i,lim;
+  int res=0, i, lim;
   lim=strLength(str);
-  for(i=0;!isEndOfLine(str[i]);i++) res+=charToInteger(str[i])*pow(10,lim-i-1);
+  for (i=0; !isEndOfLine(str[i]); i++)
+    res+=charToInteger(str[i])*pow(10,lim-i-1);
   return res;
 }
 
 
 
-void error(char* errorMessage){
+void error(char* errorMessage)
+{
   /**
    * @param char* Error Message
    */
   fprintf(stderr, "%s \n",errorMessage);
 }
 
-int readCard(char* stringCard, Carte* current){
+int readCard(char* stringCard, Carte* current)
+{
   /**
    * @param char* input card from the filereader
    * @param Carte* pointer to current card for the parser
    * @return boolean, 1 if parsed successfully, 0 if not.
    */
-  int top,bottom,left,right;
+  int id, top, bottom, left, right;
   int res;
   Carte* carte=current;
 
-  res=sscanf(stringCard,IMPORT_MASK,&top,&bottom,&left,&right);
-  /*Remplacement possible si on d�cide de se passer de la fonction interm�diaire readChar
-    Carte* carte=current;
-    res=fscanf(fp,IMPORT_MASK,&carte->Haut,&carte->Bas,&carte->Gauche,&carte->Droite);
+  res=sscanf(stringCard, IMPORT_MASK, &id, &top, &bottom, &left, &right);
 
-  */
-  /*
-    top=charToInteger(top);
-    bottom=charToInteger(bottom);
-    left=charToInteger(left);
-    right=charToInteger(right);
-  */
-  if (res!=4){
+  if (res!=5) {
     error("Incorrect card format");
     return EXIT_FAILURE;
   }
-  else{
+  else {
+    carte->identifiant = id;
     carte->Haut=top;
     carte->Bas=bottom;
     carte->Gauche=left;
@@ -119,7 +117,8 @@ int readCard(char* stringCard, Carte* current){
   return EXIT_FAILURE;
 }
 
-Carte *parseFile(char* filename,int* hauteur,int* largeur) {
+Carte *parseFile(char* filename,int* hauteur,int* largeur)
+{
   /**
    * @param char* filename to be export
    * @param int* hauteur pointer to the hauteur from the filereader
@@ -128,43 +127,36 @@ Carte *parseFile(char* filename,int* hauteur,int* largeur) {
    */
   FILE *fp;
   char ligne[MAX_BUFF+1];
-  int i=0,readedCard=0;
-  /*int hauteur=0,largeur=0;*/
-  Carte* stack;
+  int i=0, readedCard=0;
+  Carte *stack = NULL;
 
   fp = fopen(filename, "r");
+  
+  if (fp) {
+    /* lecture des dimensions pour l'allocation apres */
+    fscanf(fp," %d\n", hauteur);
+    fscanf(fp," %d\n", largeur);
 
-  if (fp){
-    fscanf(fp," %d\n",&(*hauteur));
-    fscanf(fp," %d\n",&(*largeur));
+    /* allocation du tableau de cartes et verification */
+    stack = (Carte *) malloc(sizeof(Carte)*(*hauteur)*(*largeur));
+    assert(stack);
 
-    /*
-      fgets(ligne,MAX_BUFF,fp);
-      if (strLength(ligne)==1) *hauteur=charToInteger(ligne[0]);
-      fgets(ligne,MAX_BUFF,fp);
-      if (strLength(ligne)==1) *largeur=charToInteger(ligne[0]);
-    
-    *Modif du type de sortie du parseur ! */
-     stack = (Carte *) malloc(sizeof(Carte)*(*hauteur)*(*largeur));
-	/*
-    / stack=(Carte**) malloc(sizeof(Carte)*(*hauteur)*(*largeur));
-    */
-    while(fgets(ligne,MAX_BUFF,fp)){
-      /* stack[i]=(Carte*) malloc(sizeof(Carte)); */
+    while (fgets(ligne, MAX_BUFF, fp)) {
       readedCard+=readCard(ligne,stack+i);
-      stack[i].identifiant=i;
+      /*      stack[i].identifiant=i; */
       i++;
     }
     fclose(fp);
+    
     if (readedCard!=(*hauteur)*(*largeur)) {
       error("Number of readed card and dimensions does not match");
       free(stack);
     }
+    
     return stack;
-  }
-  else{
-    error("Error: file could not be opened or found");
-  }
+  }  
+  else error("Error: file could not be opened or found");
+
   return NULL;
 }
 
@@ -177,11 +169,16 @@ void export(char* filename, Plateau *plateau){
    */
   int i, j;
   FILE* fp;
-  if((fp=fopen(filename,"w"))){
-    fprintf(fp,"%d\n%d\n",plateau->hauteur,plateau->largeur);
+  if ((fp=fopen(filename,"w"))) {
+    fprintf(fp, "%d\n%d\n", plateau->hauteur, plateau->largeur);
     for (i=0; i<plateau->hauteur; i++)
       for (j=0; j<plateau->largeur; j++)
-	fprintf(fp,EXPORT_MASK,plateau->tab[i][j]->Haut,plateau->tab[i][j]->Bas, plateau->tab[i][j]->Gauche, plateau->tab[i][j]->Droite);
+	fprintf(fp, EXPORT_MASK,
+		plateau->tab[i][j]->identifiant,
+		plateau->tab[i][j]->Haut,
+		plateau->tab[i][j]->Bas,
+		plateau->tab[i][j]->Gauche,
+		plateau->tab[i][j]->Droite);
     fclose(fp);
   }
   else error("cant't write the filename");
