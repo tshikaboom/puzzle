ec6b9105832c8c767e5dcf189ee8e6ec82f4a97f
Oskar Viljasaar <oskar.viljasaar@eesti.ee>
2013-05-22 02:24:58 +0200

    parser: prise en charge de valeurs negatives
    moulte temps passe dessus! donc va falloir que vous en soyez contents!

--- gui/inutiles/cleaner.sh
+++
@@ -1,5 +0,0 @@
-cd ..
-rm -R *x*
-rm sol1
-rm convert.pyc
-rm interface.pyc
--- src/parser.c
+++ src/parser.c
@@ -13,12 +13,24 @@
 
 #define MAX_BUFF 256
 #define INT_OFFSET 48
-#define IMPORT_MASK " %d_%1d%1d%1d%1d "
+#define IMPORT_MASK "%d"
 #define EXPORT_MASK "%d_%d%d%d%d\n"
 
 /* Les commentaires datent d'une epoque ou j'avais l'habitude de coder en anglais
  * Nettoyer les erreurs avec du perror()
  */
+
+int parse_minus(char cara)
+{
+  switch (cara) {
+  case '-':
+    return 1;
+    break;
+  default:
+    return 0;
+    break;
+  }
+}
 
 int isEndOfLine(char cara)
 {
@@ -52,6 +64,10 @@ void error(char* errorMessage)
   fprintf(stderr, "%s \n",errorMessage);
 }
 
+/* fonction readCard
+   Auteur original: Sylvain Vigano
+   Prise en charge des entiers negatifs: Oskar Viljasaar
+*/
 int readCard(char* stringCard, Carte* current)
 {
   /**
@@ -59,31 +75,76 @@ int readCard(char* stringCard, Carte* current)
    * @param Carte* pointer to current card for the parser
    * @return boolean, 1 if parsed successfully, 0 if not.
    */
-  int id, top, bottom, left, right;
-  int res;
-  Carte* carte=current;
+  int id, top, bottom, left, right,
+    ints_read=0, /* nombre de int lus, ne devrait pas depasser 4 */
+    negative_read=0; /* identifieur d'un int negatif,
+			sert surtout de "signe" pour valeur absolue pendant
+			l'assignation de valeurs */
+  unsigned int i, tmp=0;
 
-  res=sscanf(stringCard, IMPORT_MASK, &id, &top, &bottom, &left, &right);
-  #ifdef DEBUG
-  printf("Carte lue:\n");
-  printf("%d %d %d %d %d\n", id, top, bottom, left, right);
-  printf("res=%d\n",res);
-  #endif
-  
-  if (res!=5) {
+  sscanf(stringCard, IMPORT_MASK, &id);
+
+  /* on skip tout caractere avant un underscore, puis encore un autre */
+  while (stringCard[tmp] != '_') tmp++;
+  tmp++;
+
+  /* Debut du parsing en forcage */
+  for (i=tmp; i<strlen(stringCard) && ints_read<4; i++) {
+    if (stringCard[i] == '-') {
+      i++;
+      /* Si on rencontre un '-', le caractere d'apres est forcement negatif */
+      negative_read=-1;
+    }
+    else negative_read=1;
+
+    /* assignation de valeurs */
+    switch (ints_read) {
+    case 0:
+      top= negative_read*(stringCard[i]-'0');
+      break;
+    case 1:
+      bottom= negative_read*(stringCard[i]-'0');
+      break;
+    case 2:
+      left= negative_read*(stringCard[i]-'0');
+      break;
+    case 3:
+      right= negative_read*(stringCard[i]-'0');
+      break;
+    default:
+      printf("%s, %d: on n'est pas censes arriver ici\n", __FILE__, __LINE__);
+      exit(EXIT_FAILURE);
+      break;
+    }
+    ints_read++;
+  }
+#ifdef DEBUG
+  printf("Carte lue: %d %d %d %d %d\n", id, top, bottom, left, right);
+#endif
+
+  /* Check a moitie serieuse pour etre sur d'avoir des valeurs censees..
+     Remplace le resultat du sscanf qui y etait auparavant
+  */
+  if (!(top   >= -9 || top    <= 9) &&
+      (bottom >= -9 || bottom <= 9) &&
+      (left   >= -9 || left   <= 9) &&
+      (right  >= -9 || right  <= 9)) {
     error("Incorrect card format");
     return -1;
   }
+
+  /* assignation de valeurs a la carte donnee */
   else {
-    carte->identifiant = id;
-    carte->Haut=top;
-    carte->Bas=bottom;
-    carte->Gauche=left;
-    carte->Droite=right;
-    carte->rotated=0;
-    carte->sur_plateau=0;
+    current->identifiant = id;
+    current->Haut=top;
+    current->Bas=bottom;
+    current->Gauche=left;
+    current->Droite=right;
+    current->rotated=0;
+    current->sur_plateau=0;
     return 1;
   }
+
   error("Could not initiate card");
   return -1;
 }
@@ -102,7 +163,7 @@ Carte *parseFile(char* filename,int* hauteur,int* largeur)
   Carte *stack = NULL;
 
   fp = fopen(filename, "r");
-  
+
   if (fp) {
     /* lecture des dimensions pour l'allocation apres */
     fscanf(fp," %d\n", hauteur);
@@ -114,21 +175,21 @@ Carte *parseFile(char* filename,int* hauteur,int* largeur)
 
     while (fgets(ligne, MAX_BUFF, fp)) {
       readedCard+=readCard(ligne,stack+i)>0 ? 1:0;
-      /*      stack[i].identifiant=i; */
       i++;
+      /* on check si on a affaire avec des negatifs */
       #ifdef DEBUG
       printf("nb cartes lues: %d\n",readedCard);
       #endif
     }
     fclose(fp);
-    
+
     if (readedCard!=(*hauteur)*(*largeur)) {
       error("Number of readed card and dimensions does not match");
       free(stack);
     }
-    
+
     return stack;
-  }  
+  }
   else error("Error: file could not be opened or found");
 
   return NULL;
